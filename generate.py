cc57478751a2f52839294c97a05ac2338a96406d
SimonKitSangChu <kschu@ucdavis.edu>
2023-04-26 18:44:48 +0000

    Update generate.py

--- generate.py
+++ generate.py
@@ -16,9 +16,10 @@ parser.add_argument('--top_p', default=0.9, type=float, help='Top p')
 parser.add_argument('--min_length', default=300, type=int, help='Min length')
 parser.add_argument('--max_length', default=500, type=int, help='Max length')
 parser.add_argument('--num_return_sequences', default=100, type=int, help='Number of sequences to generate')
-parser.add_argument('--generation_config', default=None, type=str, help='Generation config in json')
+parser.add_argument('--generate_config', default=None, type=str, help='Generation config in json')
+parser.add_argument('--batch_size', default=1, type=int, help='Batch size in generation')
 parser.add_argument('--start_tokens', default=None, type=str, help='Start tokens')
-args, unk = parser.parse_known_args()
+args = parser.parse_args()
 
 logging.set_verbosity_info()
 logger = logging.get_logger(__name__)
@@ -30,39 +31,46 @@ def main():
     if torch.cuda.is_available():
         model = model.cuda()
 
-    if args.start_tokens is None:
-        context = '1'
-    else:
-        context = '1' + args.start_tokens if args.start_tokens[0] != '1' else args.start_tokens
-
-    tokenizer = create_tokenizer()
-    input_ids = torch.tensor(tokenizer.encode(context).ids).view([1, -1]).to(model.device)
-
-    # Generate sequences
-    generation_config = {
+    # prepare generate config
+    generate_config = {
         'do_sample': True,
         'max_length': args.max_length,
         'min_length': args.min_length,
         'top_p': args.top_p,
+        'num_return_sequences': args.batch_size,
+        'temperature': args.temperature,
     }
-    if args.generation_config is not None:
-        generation_config.update(
-            read_json(args.generation_config)
-        )
+    if args.generate_config is not None:
+        generate_config_ = read_json(args.generate_config)
+        num_return_sequences = generate_config_.pop('num_return_sequences', args.num_return_sequences)
+        if 'batch_size' in generate_config_:
+            raise ValueError('batch_size should be parsed separately from generate_config')
 
+        generate_config.update(generate_config_)
+ 
+    # prepare start tokens
+    start_tokens = generate_config.pop('start_tokens', args.start_tokens)
+    if start_tokens is None:
+        context = '1'
+    else:
+        context = '1' + start_tokens if start_tokens[0] != '1' else start_tokens
+
+    tokenizer = create_tokenizer()
+    input_ids = torch.tensor(tokenizer.encode(context).ids).view([1, -1]).to(model.device)       
+
+    # generate sequences
     sequences = []
-    for _ in tqdm(range(args.num_return_sequences // args.batch_size), desc='Generating sequences'):
+    for _ in tqdm(range(num_return_sequences // args.batch_size), desc='Generating sequences'):
         output_ids = model.generate(
             input_ids,
-            temperature=args.temperature,
-            num_return_sequences=args.batch_size,
-            **generation_config
+            **generate_config
         )
         output_ids = output_ids.cpu().numpy().tolist()
         sequences_ = tokenizer.decode_batch(output_ids)
         sequences_ = [s.lstrip('1').rstrip('2') for s in sequences_]
         sequences.extend(sequences_)
 
+    # dump sequences
     records = sequences2records(sequences)
     write_fasta(args.output_fasta, records)
 
